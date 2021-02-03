88ce967251a0dd367898a4ae99e5c040c422b986
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-03 18:59:43 +0300

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,29 @@ import (
 	"github.com/AleksandrAkhapkin/true-conf/pkg/infrastructure"
 	"github.com/AleksandrAkhapkin/true-conf/pkg/logger"
 	"github.com/pkg/errors"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+//Вам нужно написать маленькое приложение на ECHO framework предоставляющие REST API по работе с сущностью User.
+//
+//REST API должно удовлетворять следующие возможности:
+//• Добавление User //TODO OK
+//• Получение списка User
+//• Получение User по Id
+//• Редактирование User по Id
+//• Удаление User по Id
+//
+//REST API должно работать с форматом данных JSON.
+//
+//Сущность User должно состоять минимум из следующих полей:
+//• Идентификатор пользователя //TODO OK
+//• Отображаемое имя //TODO OK
+//
+//Вы можете использовать дополнительные поля, если считаете нужным.
+//
+//В качестве хранилища данных нужно использовать файл в формате JSON.
 
 func main() {
 	defer func() {
@@ -34,6 +56,19 @@ func main() {
 	if err != nil {
 		logger.LogFatal(errors.Wrap(err, "with NewHandlers"))
 	}
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+	go func() {
+		_ = <-sigChan
+		logger.LogInfo("Finish service")
+		srv.Close()
+		os.Exit(0)
+	}()
 
 	server.StartServer(handls, *port)
 
