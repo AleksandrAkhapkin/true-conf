af71d8e081d4bd2eecd957f7df03f5f9f170d768
Aleksandr Akhapkin <sashapow1@gmail.com>
2021-02-04 02:34:31 +0300

    hotfix

--- internal/server/router.go
+++ internal/server/router.go
@@ -10,11 +10,13 @@ func NewRouter(h *handlers.Handlers) *echo.Echo {
 	e := echo.New()
 
 	e.GET("/ping", h.Ping)
-	e.POST("/createuser", h.CreateUser)
-	e.GET("/users", h.GetUsers)
+
+	e.POST("/user", h.CreateUser)
 	e.GET("/user", h.GetUserByID)
-	e.PUT("/user", h.PutUserByID)
 	e.DELETE("/user", h.DeleteUserByID)
+	e.PUT("/user", h.PutUserByID)
+
+	e.GET("/users", h.GetUsers)
 
 	return e
 }
